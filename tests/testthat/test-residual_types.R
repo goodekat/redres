0003133182d20936ebc6a9acc8df51692653527f
goodekat <kgoode@iastate.edu>
2019-04-09 21:23:48 -0500

    finished tests for residual types

--- tests/testthat/test-residual_types.R
+++ tests/testthat/test-residual_types.R
@@ -1,30 +1,31 @@
 context("test-residual_types")
 
-# read data from stat510 of Iowa State University
+# read data from stat510 of Iowa State University and adjust factor variables
 d = read.delim("http://dnett.github.io/S510/SeedlingDryWeight2.txt")
-write.csv(d, "../../../seedlingdrayweight.csv", row.names = FALSE)
 d$Genotype = factor(d$Genotype)
 d$Tray = factor(d$Tray)
 d$Seedling = factor(d$Seedling)
-sas_resids <- read.csv(system.file("extdata", "residuals.csv", package = "redres"))
-sas_resids$Genotype = factor(sas_resids$Genotype)
-sas_resids$Tray = factor(sas_resids$Tray)
-sas_resids$Seedling = factor(sas_resids$Seedling)
 
 # fit a mixed model
 model <- lmer(SeedlingWeight ~ Genotype + (1|Tray), data = d)
 
-d <- dplyr::full_join(d, sas_resids, by = c("Genotype", "Tray", "Seedling", "SeedlingWeight"))
+test_that("equal to SAS", {
 
-test_that("residual types work", {
+  # Load in residuals that were computed using SAS proc mixed for comparison
+  sas_resids = read.csv(system.file("extdata", "residuals.csv", package = "redres"))
+  sas_resids$Genotype = factor(sas_resids$Genotype)
+  sas_resids$Tray = factor(sas_resids$Tray)
+  sas_resids$Seedling = factor(sas_resids$Seedling)
 
-  expect_equal(redres(model), d$ResidCond, tolerance = .00001)
-  expect_equal(redres(model, type = "raw_cond"), d$ResidCond, tolerance = .00001)
-  expect_equal(redres(model, type = "raw_mar"), d$ResidMar, tolerance = .00001)
-  expect_equal(redres(model, type = "pearson_cond"), d$PearsonResidCond, tolerance = .00001)
-  expect_equal(redres(model, type = "pearson_mar"), d$PearsonResidMar, tolerance = .00001)
+  # Join the data and SAS residuals
+  joined <- dplyr::full_join(d, sas_resids, by = c("Genotype", "Tray", "Seedling", "SeedlingWeight"))
 
-  redres(model, type = "genres")
+  # Compare redres and SAS residuals
+  expect_equal(redres(model), joined$ResidCond, tolerance = .00001)
+  expect_equal(redres(model, type = "raw_cond"), joined$ResidCond, tolerance = .00001)
+  expect_equal(redres(model, type = "raw_mar"), joined$ResidMar, tolerance = .00001)
+  expect_equal(redres(model, type = "pearson_cond"), joined$PearsonResidCond, tolerance = .00001)
+  expect_equal(redres(model, type = "pearson_mar"), joined$PearsonResidMar, tolerance = .00001)
 
 })
 
