d06c2610d810c8149baa6b283e0ff56a82fd9959
YudiZhang <yudizhang628@gmail.com>
2019-04-09 20:41:12 -0500

    finish plot

--- R/mainfunction.R
+++ R/mainfunction.R
@@ -1,42 +1,24 @@
-#' REDRES
-#' @description a package that creates diagnostic plots for mixed models.
-#' We we have four types methods to caculate residuals which are pearson residuals, generalized residuals,
-#' standardized residuals and conditional residuals. After returning residuals values,
-#' the diagnostic plots will be visualized by ggplot.
-#'
+#' Compute the Residuals
+#' @description
+#' function to compute residuals. We we have four types methods to caculate residuals,
+#' which are pearson residuals, generalized residuals, standardized residuals and
+#' conditional residuals.
+#' @usage redres(model, type = "raw_cond")
 #' @param model the model we fit from lmer function in lme4 package.
 #' @param type  type of residuals. we have generalized, standardized, conditional and pearson residuals.
 #'
 #' @export
-#'
 #' @return return residuals according to different types.
 #'
 #' @examples
-#' #get data and fit model
-#' library(lme4)
-#' x=rep(seq(0,150,by=50),12)
-#' f=factor((x+50)/50)
-#' g=factor(rep(rep(1:3,each=4),4))
-#' block=factor(rep(1:4,each=12))
-#' set.seed(532)
-#' u=c(rnorm(4,0,6),rnorm(12,0,7))
-#' e=rnorm(48,0,6)
-#' beta=c(125,15,-10,.4,-0.0015,0,.2)
-#' Z1=model.matrix(~0+block)
-#' Z2=model.matrix(~0+g:block)
-#' Z=cbind(Z1,Z2)
-#' y=X%*%beta+Z%*%u+e
-#' y=round(y,1)
-#' model=lmer(y~g*f+(1|block)+(1|block:g))
-#' #use function to get residuals
-#' result <- redres(model, type="pearson")
-#' # plot diagnostic plots for mixed models
-#' library(ggplot2)
-#' ResidPlot(model, result)
+#' #fit the model
+#' fm1 <- lmer(Reaction ~ Days + (Days | Subject), sleepstudy)
+#' #compute the residuals
+#' result <- redres(model, type = "raw_cond")
 
-redres <- function(model, type="raw_cond"){
+redres <- function(model, type = "raw_cond"){
 
-  assertthat::assert_that(is.character(type), msg='type must be a string.')
+  assertthat::assert_that(is.character(type), msg = 'type must be a string.')
   stopifnot(class(model)[1] == "lmerMod")
 
   if(type=="raw_cond"){
@@ -54,7 +36,7 @@ redres <- function(model, type="raw_cond"){
   else if(type=="std_cond"){
     result <- stdres(model, cond = TRUE)
   }
-  else if(type=="std_mard"){
+  else if(type=="std_mar"){
     result <- stdres(model, cond = FALSE)
   }
   else if(type=="genres"){
@@ -66,7 +48,6 @@ redres <- function(model, type="raw_cond"){
 
   # Return the residuals
   return(result)
-
 }
 
 # usethis::use_testthat()
