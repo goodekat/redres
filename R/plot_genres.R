40cb7aff976c901bfb2c43cde32a350276c6b6e2
kmcclernon <kelliem@iastate.edu>
2019-04-13 14:22:05 -0500

    update plot_genres documentation

--- R/plot_genres.R
+++ R/plot_genres.R
@@ -1,18 +1,22 @@
-# plots generalized residuals against uniform quantiles
-
-#' @import ggplot2
-#' @import qqplotr
+#' Generalized residual quantile plot
+#' @description
+#' Use to compare the generalized residuals to the uniform quantiles. Obvious departures indicate
+#' a lack of fit.
+#'
+#' @param model
+#' @importFrom  ggplot2 aes_string ggplot theme_bw xlab ylab
+#' @importFrom qqplotrstat_qq_band stat_qq_line stat_qq_point
 # Notes: not sure what all I should import from ggplot2 and qqplotr
 # Also need to pick a color palette
 
-plot_genres <- function(model, bandType = "ts"){
+plot_genres <- function(model){
 
   # building dataframe
   g_df <- data.frame(y = model@resp$y, Residual = genres(model))
 
   # quantile plot from qqplotr
-  ggplot(data = g_df, aes(sample = Residual)) +
-    qqplotr::stat_qq_band(bandType = bandType, fill = "#8DA0CB",
+  ggplot(data = g_df, aes_string(sample = "Residual")) +
+    qqplotr::stat_qq_band(bandType = "ts", fill = "#8DA0CB",
                           alpha = 0.4, distribution = "unif") +
     qqplotr::stat_qq_line(colour = "#8DA0CB", distribution = "unif") +
     qqplotr::stat_qq_point(distribution = "unif") +
