b4e0caa04f6a2f0959858f85c82e419b1d3353f5
goodekat <kgoode@iastate.edu>
2019-04-22 11:41:42 -0500

    added app to R file and created function to run the app

--- R/runExample.R
+++
@@ -1,9 +0,0 @@
-#' @export
-runExample <- function() {
-  appDir <- system.file("shiny", package = "redres")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `redres`.", call. = FALSE)
-  }
-
-  shiny::runApp(appDir, display.mode = "normal")
-}
