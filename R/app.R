3107987f75854e5893d9f5b53166685752c8c888
jingzhao19 <zhaojing1010101@163.com>
2019-04-25 12:36:01 -0500

    update shiny app

--- R/app.R
+++ R/app.R
@@ -1,18 +1,40 @@
-#' App for 'redressing' linear mixed models
+
+#' Shiny App for 'redressing' linear mixed models
 #'
 #' @description
-#' app app
+#' This is a shiny app for model diagnoistic of mixed model using \code{lmer} function from lme4 package.
+#' This app includes residual plot computed from six types of residuals (conditional raw, Pearson, and
+#' studentized, marginal raw, Pearson, and studentized, and generalized residuals) and QQ plot using
+#' generalized residual. This app can also be used to do model selection through pairwise comparison
+#' two models from different linear mixed models using lmer function.
 #'
-#' @param model Model fit using \code{lmer}.
+#' @param model Model fit using \code{lmer} function from lme4 package.
 #'
-#' @importFrom plotly ggplotly plotlyOutput renderPlotly
+#' @usage redres_app(model=name_model)
 #'
+#' @importFrom cowplot plot_grid
 #' @import shiny
+#'
 #' @export redres_app
+#'
+#'
+#' @examples
+#' # fits a linear mixed effects model
+#' library(lme4)
+#' fm1 <- lmer(Reaction ~ Days + (Days | Subject), data = sleepstudy)
+#' redres_app(model=fm1)
+
+#' # comparing two different linear mixed effects models
+#' fm1 <- lmer(Reaction ~ Days + (Days | Subject), data = sleepstudy)
+#' fm2 <- lmer(Reaction ~ Days + (1|Subject) + (0+Days|Subject), sleepstudy)
+#' cmbd <- c(fm1,fm2)
+#' redres_app (model=cmbd )
+
+
 
 
 # Function for running shiny app
-redres_app <- function(model) {
+redres_app <- function(model= name_model) {
 
   ui <- ui_fun()
   server <- server_fun(model)
@@ -33,99 +55,51 @@ ui_fun <- function(){
                     p("group 6", style="font-size:30px")),
         align = "center", style="color:#ffffff; background-color: #4d728d"),
 
-    # tabPanel("Data Import",
-    #          sidebarLayout(
-    #            sidebarPanel(
-    #              fileInput("file","Upload your CSV File", multiple = FALSE),
-    #              tags$hr(),
-    #              h5(helpText("Select the read.table parameters below")),
-    #              checkboxInput(inputId = 'header', label = 'Header', value = FALSE),
-    #              radioButtons(inputId = 'sep', label = 'Separator',
-    #                           choices = c(Comma=',',Semicolon=';',Tab='\t', Space=''), selected = ',')),
-    #            mainPanel(uiOutput("tb1")))),
+    tabPanel("About"),
 
-    # tabPanel("Model",
-    #          sidebarLayout(sidebarPanel(
-    #            uiOutput("res_select"),
-    #            uiOutput("fixed_select"),
-    #            uiOutput("random_select")),
-    #            mainPanel(helpText("Your selected variables"),
-    #                      verbatimTextOutput("other_val_show")))),
 
     tabPanel("Plots",
              sidebarLayout(sidebarPanel(
-               selectInput("residual_type", "Residual Type",
+               selectInput("residual_type", "Residual type",
                            choices = c("raw_cond", "raw_mar",
                                        "pearson_cond", "pearson_mar",
                                        "std_cond", "std_mar"), selected = "raw_cond")),
                mainPanel(
                  tabsetPanel(
-                   tabPanel("Residual Plot", plotlyOutput("resid")),
+                   tabPanel("Residual Plot", plotOutput("resid")),
                    tabPanel("Generalized Residual Quantile Plot",
-                            plotOutput("quantile"))))
+                            plotOutput("quantile"))
+                 ))
              ))
   )
 
 }
 
 
-server_fun <- function(model){
+server_fun <- function (model) {
 
-  shiny::shinyServer(function(input, output) {
-    # data <- reactive({
-    #   if (is.null(input$file)) {
-    #     return(NULL)
-    #   }
-    #   read.table(file = input$file$datapath, sep = input$sep,
-    #              header = input$header)
-    # })
-    #
-    # output$table <- renderTable({
-    #   if(is.null(data())){return ()}
-    #   data()
-    # })
-    # output$tb1 <- renderUI({
-    #   tableOutput("table")
-    # })
-    # output$res_select <- renderUI({
-    #   selectInput("res_select","Select Response",
-    #               choices = as.list(names(data())), multiple = FALSE)
-    # })
-    # output$fixed_select <- renderUI({
-    #   selectInput("fixed_select","Select Fixed Effect",
-    #               choices = as.list(names(data())), multiple = FALSE)
-    # })
-    # output$random_select <- renderUI({
-    #   selectInput("random_select","Select Random Effect",
-    #               choices = as.list(names(data())), multiple = FALSE)
-    # })
-    #
-    # output$other_val_show <- renderPrint({
-    #   input$res_select
-    #   input$fixed_select
-    #   input$random_select
-    #   f <- data()
-    #
-    #   form <- sprintf("%s~%s", input$res_select,
-    #                   paste0(
-    #                     input$fixed_select, "+", "(",
-    #                     input$fixed_select, "|",
-    #                     input$random_select, ")"))
-    #   print(form)
-    #
-    #   model <- lmer(as.formula(form), data = f)
-    #   print(summary(model))
-    # })
+  shiny::shinyServer( function(input, output) {
 
-    output$resid <- renderPlotly({
-      rplot <- plot_redres(model, input$residual_type)
-      ggplotly(rplot)
+    output$resid <- renderPlot({
+
+      if (length(model) == 1){
+        plot_redres(model, input$residual_type)
+      } else {
+        m1_resid <- plot_redres(model[[1]], input$residual_type) + xlab("model_1")
+        m2_resid <- plot_redres(model[[2]], input$residual_type) + xlab("model_2")
+        plot_grid(m1_resid,m2_resid)
+      }
     })
-
     output$quantile <- renderPlot({
-      plot_genres(model)
+      if (length(model) == 1){
+        plot_genres(model)
+      } else {
+        m1_qq <- plot_genres(model[[1]]) + xlab("model_1")
+        m2_qq <- plot_genres(model[[2]]) + xlab("model_2")
+        plot_grid(m1_qq,m2_qq)
+      }
     })
   })
-
 }
+
 
