c5451331b7407452bd46758bd2ba4f367f8cae80
goodekat <kgoode@iastate.edu>
2019-04-26 16:00:09 -0500

    documented the paprika dataset (it should be finished now!)

--- R/data_paprika.R
+++ R/data_paprika.R
@@ -1,31 +1,31 @@
 #' Paprika Dataset
 #'
 #' Data from a study on the growth rate of paprika plants grown under small-scale farming
-#' conditions in southern Africa. The study was conducted in 2007 and 2008 in Malawi. The
-#' researcher compared 4 fertilizer treatments and 6 plant varieties.
+#' conditions in southern Africa.
 #'
+#' @format A tibble with 1070 rows and 5 variables:
+#' \describe{
+#'   \item{rep}{replicate number}
+#'   \item{treatment}{fertilizer treatment}
+#'   \item{variety}{plant variety}
+#'   \item{plant}{plant number}
+#'   \item{height}{height of the plant (cm)}
+#'   ...
+#' }
 #'
-#' ((1) control, (2) inorganic fertilizer,
-#' (3) Gliricidia biomass, and (4) Gliricidia biomass plus half the recommended dose of
-#' fertilizer for paprika) and 6 varieties of plant ((1) Papri-King, (2) Papri-Queen,
-#' (3) Papri-Excel, (4) Papri-Supreme, (5) Papri-Ace, and (6) PX1140 4601.
+#' @details The study was conducted in 2007 and 2008 in Malawi using a split plot design
+#' with five replicates. The researchers compared 4 fertilizer treatments ((1) control,
+#' (2) inorganic fertilizer, (3) Gliricidia biomass, and (4) Gliricidia biomass plus
+#' half the recommended dose of fertilizer for paprika) and 6 plant varieties
+#' ((1) Papri-King, (2) Papri-Queen, (3) Papri-Excel, (4) Papri-Supreme, (5) Papri-Ace,
+#' and (6) PX1140 4601). The fertilizers were applied to the main plots, and the
+#' varieties were assigned to the subplots within a main plot. For each replicate,
+#' 10 plants were randomly chosen from each treatment and variety treatment combination
+#' and observed over 20 weeks. This dataset contains observations from week 4
+#' of the data in 2008.
 #'
-#' The
-#' experimental design of the study was a split-plot with 5 replicates.
-#'
-#' This dataset contains observations from week 4 of the
-#' study in 2008.
-#'
-#' @details The variables in the dataset are as follows.
-#' \tabular{ll}{
-#'  \code{heartrate} \tab Heart rate of the penguin during the dive (beats per minute) \cr
-#'  \code{depth} \tab Depth of the dive (meters) \cr
-#'  \code{duration} \tab Duration of the dive (minutes) \cr
-#'  \code{bird} \tab The id number associated with a penguin}
-#'
-#' @format Tibble
-#'
-#' @source The data was accessed through the Harvard dataverse: \url{https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/25747} and the original paper can be found here: \url{https://link.springer.com/article/10.1007%2Fs10457-011-9415-2}
+#' @source The data was obtained from \url{https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/25747},
+#' and the original paper on the study can be found at \url{https://link.springer.com/article/10.1007\%2Fs10457-011-9415-2}.
 
 "paprika"
 
--- data-raw/paprika.R
+++ data-raw/paprika.R
@@ -13,7 +13,11 @@ paprika_raw <- read_excel("data-raw/Height+growth2008.xlsx",
 paprika <- paprika_raw %>%
   filter(Week == 4) %>%
   select(-Year, -Week, -`# leaves`, -`# branches`, -`# Fruits`) %>%
-  rename(rep = Rep, treatment = Treatmnt, variety = Variety, plant = `Plant #`, height = Height)
+  rename(rep = Rep, treatment = Treatmnt, variety = Variety, plant = `Plant #`, height = Height) %>%
+  mutate(rep = factor(rep),
+         treatment = factor(treatment),
+         variety = factor(variety),
+         plant = factor(plant))
 
 # Add the puerto_rico to the data folder
 #usethis::use_data(paprika, overwrite = TRUE)
